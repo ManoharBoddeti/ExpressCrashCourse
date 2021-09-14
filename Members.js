babe331211ccbd648f797ca806820359b5635455
ManoharBoddeti <manoharboddeti8252@gmail.com>
2021-09-14 12:23:08 +0530

    first commit

---
+++ Members.js
@@ -0,0 +1,22 @@
+const members = [
+    {
+        id:1,
+        name: 'John Doe',
+        emial: 'john@gmail.com',
+        status: 'active'
+    },
+    {
+        id:2,
+        name: 'Jane',
+        emial: 'jane@gmail.com',
+        status: 'inactive'
+    },
+    {
+        id:1,
+        name: ' Doe',
+        emial: 'deo@gmail.com',
+        status: 'active'
+    },
+];
+
+module.exports = members;
