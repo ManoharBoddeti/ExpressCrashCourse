f9b2e0f3c0a1fed417d78461d17e512f76f12a4c
MANOHAR <88959204+ManoharBoddeti@users.noreply.github.com>
2021-10-18 12:52:48 +0530

    Update Members.js

--- Members.js
+++ Members.js
@@ -2,19 +2,19 @@ const members = [
     {
         id:1,
         name: 'John Doe',
-        emial: 'john@gmail.com',
+        email: 'john@gmail.com',
         status: 'active'
     },
     {
         id:2,
         name: 'Jane',
-        emial: 'jane@gmail.com',
+        email: 'jane@gmail.com',
         status: 'inactive'
     },
     {
         id:1,
         name: ' Doe',
-        emial: 'deo@gmail.com',
+        email: 'deo@gmail.com',
         status: 'active'
     },
 ];
